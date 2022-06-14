946fefdf219ff13447c1d96bd83d2caeb66012e6
allencode11 <al.el.en.lina@gmail.com>
2022-06-14 23:50:18 +0300

    adding display property exercises

---
+++ ex2-displayProp/ex2.scripts.js
@@ -0,0 +1,34 @@
+const exponents = document.getElementsByTagName('sub');
+const powers =  document.getElementsByTagName('sup');
+
+for(let i = 0; i < exponents.length; i++) {
+  exponents[i].style.color = 'blue';
+};
+
+for(let i = 0; i < powers.length; i++) {
+  powers[i].style.color = 'red';
+};
+
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const ex1 = document.getElementById('ex1');
+const ex2 = document.getElementById('ex2');
+const ex3 = document.getElementById('ex3');
+
+const btn1Click = () => {
+  ex1.classList.remove('hidden');
+  ex2.classList.add('hidden');
+  ex3.classList.add('hidden');
+};
+
+const btn2Click = () => {
+  ex2.classList.remove('hidden');
+  ex1.classList.add('hidden');
+  ex3.classList.add('hidden');
+};
+
+const btn3Click = () => {
+  ex3.classList.remove('hidden');
+  ex1.classList.add('hidden');
+  ex2.classList.add('hidden');
+};
